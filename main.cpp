fd05c20073f52d422e80ef2ced1f6be948a78684
Aditya Prakash <adiitya.adda@gmail.com>
2016-03-02 15:48:39 +0530

    Added function definitions for capturing and analysing packets. Printing results

--- main.cpp
+++ main.cpp
@@ -16,8 +16,16 @@
 #include <cstdlib>
 
 #define MAX_TIME 2
-using namespace std;
 
+using namespace std;
+/*
+HOW IT WORKS:
+1. After connecting to the device it continuously hops between different channels.
+   It randomly chooses another channel till that channel has spent 1 minute time in total.
+   After a channel has complete result it prints it till result is done for each channel
+   and Finally result is obtained.
+   Rest the code is commented for better understanding.
+*/
 
 int time_spent[13];//Defines how much time has been spent on a channel
 bool printed[13];//Is a channel result printed or not.
@@ -37,33 +45,129 @@ struct ether_header *eptr;  /* net/ethernet.h */
 char* temp1;
 string temp2;
 
-//Prints the global result
-void print_observation() {
+/*
+    This function captures any packet coming in the current channel
+    and analyses it and feeds it in our data set.
+*/
+void capture_and_analyse(const u_char* packet, int len) {
 
+
+    temp1 = new char[100];
+    temp2 = "";
+    int start;
+
+    bool toDS = packet[19] & (1 << 8);
+    bool fromDS = packet[19] & (1 << 7);
+
+    if( fromDS==0)
+        start = 28;
+    else if(toDS==1 && fromDS==1)
+        start = 42;
+    else
+        start = 34;
+    //Finds from
+    //Gets the part having mac_id
+    for(int k=start; k <= start+5; k++){
+       sprintf(temp1,"%02X", packet[k]);
+       temp2 += temp1[0];
+       temp2 += temp1[1];
+       if(k!=start+5)
+          temp2 += ":";
+    }
+    time_t curr = time(NULL);
+    cout << "Mac_id " << temp2 << " received at " << asctime(localtime(&curr)) ;
+
+}
+
+//Prints the global result
+void print_observation()
+{
+    cout<< "Output in terms of number of packets captured : "<<endl;
+    for(int i=0;i<13;i++)
+        printf("Channel %d received total %d packets\n",i+1,packets[i]);
+    cout << endl;
+    cout << "Across all channels : ";
+    cout << endl;
+    for(map<string,int>::iterator itr = global_mac.begin(); itr != global_mac.end(); itr++){
+       cout<<"Mac_id " << (*itr).first << " has appeared " << (double)((*itr).second)/13.0 << " times per minute" << endl;
+    }
+    cout << endl;
+    exit(0);
 }
 
 //Calls the packet capturer for each channel
 void handle_capture(const struct pcap_pkthdr* pkthdr,const u_char*
         packet)
 {
-
+    capture_and_analyse(packet, pkthdr->len);
 }
 
 //Prints the output collected per channel
-void printChannel(int channel) {
+void printChannel(int channel)
+{
+    printed[channel]=true;
+    cout<<"Number of packets received in channel "<< channel+1 <<" : "<<packets[channel]<<endl;
+    cout << endl;
+    for(map<string,int>::iterator itr = mac_count[channel].begin();itr!=mac_count[channel].end();itr++)
+    {
+        cout<<"Mac_id " << (*itr).first << " " << " has been received " << (*itr).second << " time(s) in channel " << channel+1 << endl;
+    }
+    cout << endl;
+    int counter = 0;
+    for(int i=0;i<13;i++){
+        if(!printed[i])
+            counter++;
+        if(counter==1)
+            print_observation();
+    }
 
 }
 
 //Gives a channel for which complete 1 min is not spent and if so it prints result for it.
 int getChannel()
 {
-
+    int c1 = rand()%13;
+    if(time_spent[c1] < 6)
+    {
+        return c1;
+    }
+    else
+    {
+        if(!printed[c1])
+            printChannel(c1);
+        printed[c1] = true;
+        return -1;
+    }
 }
 
 //Changes channel and listens there for MAX_TIME
-void core_channel(int cid) {
+void core_channel(int cid){
+    //change to channel
+    string channel_str = "iw dev wlan2 set channel ";
+    char* tem = new char[2];
+    sprintf(tem, "%d", cid+1);
+    channel_str += tem;
+    cout<<"Scanning channel "<< cid + 1 << "..." << endl;
+    cout << endl;
+    system(channel_str.c_str());
+    time_t end = time(NULL) + MAX_TIME;
+    while(time(NULL) <= end){
+        //pcap_loop(descr,handle_capture,NULL);
+        struct pcap_pkthdr hdr;
+        u_char* packet = (u_char*)pcap_next(descr,&hdr);
+        if(packet==NULL)
+            continue;
+        handle_capture(&hdr,packet);
+        packets[cid]++;
 
+        mac_count[cid][temp2]++;
+        global_mac[temp2]++;
+    }
+    cout << endl;
+    time_spent[cid] += MAX_TIME;
 }
+
+
 
 //Starts the working of program
 void start()
@@ -109,5 +213,6 @@ int main(int argc, char** argv)
     start();
     return 0;
 }
+
 
 
